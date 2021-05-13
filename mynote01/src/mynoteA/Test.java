21fb3619e1dec8c90bc7f7225a451ec3ac42804d
Your Name <you@example.com>
2021-05-13 09:26:58 +0800

    我写了一段程序

---
+++ mynote01/src/mynoteA/Test.java
@@ -0,0 +1,12 @@
+package mynoteA;
+
+/**
+ * @Auther:wangjg
+ * @Date:2021/5/13 - 05 - 13 - 8:59
+ * @Description: mynoteA
+ * @version: 1.0
+ */
+public class Test {
+    private int age;
+    private String name;
+}
