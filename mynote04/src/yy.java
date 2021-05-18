30cb53e97aefc3db5006e579d0cdfafe89ecca40
Your Name <you@example.com>
2021-05-18 16:34:29 +0800

    茶店系统

---
+++ mynote04/src/yy.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * @Auther:wangjg
+ * @Date:2021/5/18 - 05 - 18 - 11:04
+ * @Description: PACKAGE_NAME
+ * @version: 1.0
+ */
+public class yy {
+    public static void main(String[] args) {
+        Scanner ji = new Scanner(System.in);
+        String s = ji.next();
+        System.out.println(s);
+    }
+}
